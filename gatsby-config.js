d90eb53e97388b8c92b36a3ab3496aac2a101237
Kamran Ali <kamranalinitb@gmail.com>
2019-09-11 10:34:51 +0530

    SEO  CSP headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,10 +65,10 @@ module.exports = {
         mergeStyleHashes: false, 
         mergeDefaultDirectives: true,
         directives: {
-          "default-src": "'self' https://disqus.com https://c.disquscdn.com www.google-analytics.com www.google-analytics.com/analytics.js",
-          "script-src": "'self' 'unsafe-inline' www.google-analytics.com https://kamranali.disqus.com", //<- 'unsafe-inline' is unsafe and is required by Disqus
-          "style-src": "'self' 'unsafe-inline' https://c.disquscdn.com", //<- "'unsafe-inline'" should be avoided but the plugin was broken with mergeStyleHashes
-          "img-src": "'self' data: www.google-analytics.com https://referrer.disqus.com https://c.disquscdn.com"
+          "default-src": "'self' disqus.com marketingplatform.google.com/about/analytics/ c.disquscdn.com www.google-analytics.com www.google.com/analytics/",
+          "script-src": "'self' 'unsafe-inline' www.google-analytics.com kamranali.disqus.com", //<- 'unsafe-inline' is unsafe and is required by Disqus
+          "style-src": "'self' 'unsafe-inline' c.disquscdn.com", //<- "'unsafe-inline'" should be avoided but the plugin was broken with mergeStyleHashes
+          "img-src": "'self' data: www.google-analytics.com referrer.disqus.com c.disquscdn.com"
         }
       }
     }
