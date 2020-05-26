9c33a0bfe646a993786bd6032b6a37c1d09409dd
Kamran Ali <kamranalinitb@gmail.com>
2020-05-26 23:50:45 +0530

    Added mailchimp URL in form-action

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,8 @@ module.exports = {
           "default-src": "'self' disqus.com marketingplatform.google.com/about/analytics/ c.disquscdn.com www.google-analytics.com www.google.com/analytics/",
           "script-src": "'self' 'unsafe-inline' www.google-analytics.com kamranali.disqus.com", //<- 'unsafe-inline' is unsafe and is required by Disqus
           "style-src": "'self' 'unsafe-inline' c.disquscdn.com", //<- "'unsafe-inline'" should be avoided but the plugin was broken with mergeStyleHashes
-          "img-src": "'self' data: www.google-analytics.com referrer.disqus.com c.disquscdn.com"
+          "img-src": "'self' data: www.google-analytics.com referrer.disqus.com c.disquscdn.com",
+          "form-action": "'self' https://kamranali.us17.list-manage.com/subscribe/post?u=835b966c8e4fb4811d20a1b0c&amp;id=1ccb85525c"
         }
       }
     }
